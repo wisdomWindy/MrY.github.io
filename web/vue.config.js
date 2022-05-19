d9d9c6f510208ade917c9bbdfb3a84902d661b8c
淼 <oncwnuAY3AEycYlfemOQQ1CvcijQ@git.weixin.qq.com>
2022-05-19 17:33:12 +0800

    添加配置

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'../dist'
+  outputDir:'../dist',
+  publicPath:'../dist'
 })
