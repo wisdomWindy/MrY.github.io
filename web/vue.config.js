edf20a0b68387cc65c1a23c697fb7a1f93261697
淼 <oncwnuAY3AEycYlfemOQQ1CvcijQ@git.weixin.qq.com>
2022-05-19 18:02:24 +0800

    调整配置

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'../dist',
-  publicPath:'./dist/'
+  outputDir:'../dist/',
+  publicPath:'./dist/', // 以index.html为参考点
+  assetsDir:'./static/', // 相对于outputDir  ../dist/./static/
+  indexPath:'../index.html', // 相对于outputDir ../dist/../index.html
 })
