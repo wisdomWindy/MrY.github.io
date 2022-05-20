065cd45ba72f07d8a8fb4d0bbbcefecb735a9001
淼 <oncwnuAY3AEycYlfemOQQ1CvcijQ@git.weixin.qq.com>
2022-05-20 11:07:03 +0800

    调整配置

--- web/README.md
+++ web/README.md
@@ -22,3 +22,20 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 部署
+
+pubilcPath：服务器访问目录
+
+outputDir：项目打包后的输出目录，相对于pubilcPath(publicPath/outputDir)
+
+assetsDir：相对于outputDir(outputDir/assetsDir)
+
+indexPath是相对于outputDir的（outputDir/filename）
+
+#### 开发环境
+
+
+
+#### 生产环境
+
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,9 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
+// 服务器根目录 web目录用ROOT表示：ROOT/web
+const ROOT = path.resolve(__dirname, '..');
+
+// 打包输出文件目录
+const outputDir = path.join(ROOT, 'dist/');
+
+// 服务器访问目录
+const publicPath = path.join(ROOT);
+
+// 静态文件输出目录
+const assetsDir = path.join(ROOT, 'static/');
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'/dist',
-  assetsDir:'./static/', // 相对于outputDir  ../dist/./static/
-  indexPath:'../',
+  outputDir:'/dist/', // 输出文件的目录为根目录下的dist
+  assetsDir: './static/', // 相对于outputDir  ../dist/./static/
+  publicPath:'/', // 访问的地址是服务器根目录下的dist
   lintOnSave:false,
   productionSourceMap:false,
   chainWebpack: config => {
