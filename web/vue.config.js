9b76d5d8f05ca0c9705d2d7c7c01df8fc31f85a0
淼 <oncwnuAY3AEycYlfemOQQ1CvcijQ@git.weixin.qq.com>
2022-05-20 14:50:27 +0800

    调整配置

--- web/vue.config.js
+++ web/vue.config.js
@@ -13,12 +13,12 @@ const publicPath = path.join(ROOT);
 // 静态文件输出目录
 const assetsDir = path.join(ROOT, 'static/');
 
-
+// vue中public文件夹放在与src同级目录下，该目录中放置静态资源，引用时路径相对于实际引用的文件
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:'../dist/', // 输出文件的目录为根目录下的dist
   assetsDir: './', // 相对于outputDir  ../dist/./
-  publicPath:'/dist/',
+  publicPath: '/dist/', // 打包后的引入资源地址的目录
   indexPath:'../index.html',
   lintOnSave:false,
   productionSourceMap:false
