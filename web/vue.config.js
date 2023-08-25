42b4294627b715284215651d15151893065c32f5
wisdomwindy <1659140773@qq.com>
2023-08-25 18:00:18 +0800

    使用cdn

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,42 +1,59 @@
 const { defineConfig } = require('@vue/cli-service');
+const WebpackCDNPlugin = require('webpack-cdn-plugin');
 const timeStamp = Date.now();
 // vue中public文件夹放在与src同级目录下，该目录中放置静态资源，引用时路径相对于实际引用的文件
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'../dist/', // 输出文件的目录为根目录下的dist
+  outputDir: '../dist/', // 输出文件的目录为根目录下的dist
   assetsDir: './', // 相对于outputDir  ../dist/./
-  indexPath:'../index.html',
-  publicPath:'/dist/',
-  lintOnSave:false,
-  productionSourceMap:false,
-  configureWebpack:(config)=>{
+  indexPath: '../index.html',
+  publicPath: '/dist/',
+  lintOnSave: false,
+  productionSourceMap: false,
+  configureWebpack: (config) => {
     config.output.filename = `js/[name].[chunkhash].${timeStamp}.js`;
     config.output.chunkFilename = `js/[name].[chunkhash].${timeStamp}.js`;
     // 清除console代码
-    let optimizationCompress = config.optimization.minimizer[0].options.minimizer.options.compress ;
-    config.optimization.minimizer[0].options.minimizer.options.compress = Object.assign(optimizationCompress,{
-      drop_console:true,
-      drop_debugger:true
+    let optimizationCompress = config.optimization.minimizer[0].options.minimizer.options.compress;
+    config.optimization.minimizer[0].options.minimizer.options.compress = Object.assign(optimizationCompress, {
+      drop_console: true,
+      drop_debugger: true
     });
+    config.plugins.push(new WebpackCDNPlugin({
+      modules: [{
+        name: 'vue',
+        var: 'Vue',
+        url: 'dist/vue.runtime.min.js'
+      }, {
+        name: 'vue-router',
+        var: 'VueRouter',
+        url: 'dist/vue-router.runtime.min.js'
+      }, {
+        name: 'vuex',
+        var: 'Vuex',
+        url: 'dist/vuex.runtime.min.js'
+      }],
+      publicPath:'/node_modules'
+    }))
   },
-  chainWebpack:(config)=>{
+  chainWebpack: (config) => {
     config.plugin('extract-css').tap(arg => [{
-      filename:`css/[name].${timeStamp}.css`,
+      filename: `css/[name].${timeStamp}.css`,
       chunkFilename: `css/[name].${timeStamp}.css`
     }]);
   },
-  css:{
-    extract:{
-      ignoreOrder:true
+  css: {
+    extract: {
+      ignoreOrder: true
     }
   },
-  devServer:{
-    static:{
-      publicPath:'/dist/dist/images/', // 访问服务器的url，当访问该url时返回对应的资源
-      directory:'./public/images/' // 提供资源的文件夹，当访问publicPath时提供directory中的对应内容
+  devServer: {
+    static: {
+      publicPath: '/dist/dist/images/', // 访问服务器的url，当访问该url时返回对应的资源
+      directory: './public/images/' // 提供资源的文件夹，当访问publicPath时提供directory中的对应内容
     },
-    client:{
-      overlay:false
+    client: {
+      overlay: false
     }
   }
 })
