a1e2cb442e9d4ccacd3069ff7208500d130ad21f
wisdomwindy <1659140773@qq.com>
2023-08-30 14:05:55 +0800

    取消CDN

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
-const WebpackCDNPlugin = require('webpack-cdn-plugin');
+// const WebpackCDNPlugin = require('webpack-cdn-plugin');
 const timeStamp = Date.now();
 // vue中public文件夹放在与src同级目录下，该目录中放置静态资源，引用时路径相对于实际引用的文件
 module.exports = defineConfig({
@@ -13,30 +13,30 @@ module.exports = defineConfig({
   configureWebpack: (config) => {
     config.output.filename = `js/[name].[chunkhash].${timeStamp}.js`;
     config.output.chunkFilename = `js/[name].[chunkhash].${timeStamp}.js`;
-    config.externals = {
-      'vue': 'Vue',
-      'element-plus':'Element',
+    // config.externals = {
+    //   'vue': 'Vue',
+    //   'element-plus':'Element',
       
-    };
+    // };
     // 清除console代码
     let optimizationCompress = config.optimization.minimizer[0].options.minimizer.options.compress;
     config.optimization.minimizer[0].options.minimizer.options.compress = Object.assign(optimizationCompress, {
       drop_console: true,
       drop_debugger: true
     });
-    config.plugins.push(new WebpackCDNPlugin({
-      modules: [{
-        name: 'vue',
-        var: 'Vue',
-        path: 'dist/vue.runtime.global.prod.js'
-      },{
-        name: 'element-plus',
-        var: 'Element',
-        path: 'dist/index.full.min.js'
-      }],
-      publicPath:'/node_modules',
-      prod:true
-    }));
+    // config.plugins.push(new WebpackCDNPlugin({
+    //   modules: [{
+    //     name: 'vue',
+    //     var: 'Vue',
+    //     path: 'dist/vue.runtime.global.prod.js'
+    //   },{
+    //     name: 'element-plus',
+    //     var: 'Element',
+    //     path: 'dist/index.full.min.js'
+    //   }],
+    //   publicPath:'/node_modules',
+    //   prod:true
+    // }));
   },
   chainWebpack: (config) => {
     config.plugin('extract-css').tap(arg => [{
