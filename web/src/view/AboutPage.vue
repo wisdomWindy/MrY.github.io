31c2a661f770ef2d916b4eae78053328c1218c8b
wisdomwindy <1659140773@qq.com>
2023-09-07 17:58:28 +0800

    添加随机文本接口

--- web/src/view/AboutPage.vue
+++ web/src/view/AboutPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="contianer">
+    <p>{{ sentense }}</p>
     <h2>个人介绍</h2>
     <div class="self-description">
       鄙人于2021年从事前端开发职业，距今已有{{ timeSpan }}年。主要技术栈是HTML、CSS、JavaScript、Vue、React、typescript、webpack、小程序。
@@ -9,21 +10,15 @@
       <el-link class="link" v-for="(link,index) in links" :key="index" :href="link.href" :title="link.title" :underline="false">
         <span :class="['iconfont',link.icon]"></span>
       </el-link>
-      <!-- <el-link class="link" href="mailto:me@1659140773@qq.com" title="email" :underline="false">
-        <span class="iconfont icon-email"></span>
-      </el-link>
-      <el-link class="'link'" title="qq" href="https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi" :underline="false">
-        <span class="iconfont icon-QQ"></span>
-      </el-link> -->
   </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref,onBeforeMount} from 'vue'
 let startDate = new Date('2021/03/08');
 let timeSpan = new Date().getFullYear() - startDate.getFullYear();
-
+let sentense = ref('');
 let links = [{
   href:'https://github.com/wisdomWindy',
   title:'github',
@@ -36,7 +31,14 @@ let links = [{
   href: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
   title: 'qq',
   icon: 'icon-QQ',
-}]
+}];
+onBeforeMount(()=>{
+  fetch('https://el-bot-api.elpsy.cn/api/words/young').then(res => {
+    return res.json();
+  }).then(result => {
+   sentense.value = result[0];
+  });
+});
 </script>
 
 <style scoped>
