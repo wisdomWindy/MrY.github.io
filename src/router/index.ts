dd3b091e8d9e5847c5416aec8ff4aacb564ee322
wisdomwindy <1659140773@qq.com>
2023-11-08 10:47:34 +0800

    新增路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,54 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-    path: '/',
-    redirect: '/home'
-  }, {
-    path: '/home',
-    name: 'home',
-    component: () => import('@/views/Home.vue')
-  }, {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/About.vue')
-  }, {
-    path: '/tool',
-    name: 'tool',
-    component: () => import('@/views/tools/Tool.vue'),
-    children: [{
-      path: 'videoTransform',
-      name: 'videoTransform',
-      component: () => import('@/views/tools/VideoTransform.vue')
-    }, {
-      path: 'base64',
-      name: 'base64',
-      component: () => import('@/views/tools/Base64.vue')
-    }, {
-      path: 'docxpreview',
-      name: 'docxpreview',
-      component: () => import('@/views/tools/Docxpreview.vue')
-    }]
-  }
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('@/views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/About.vue')
+    },
+    {
+      path: '/tool',
+      name: 'tool',
+      component: () => import('@/views/tools/Tool.vue'),
+      children: [
+        {
+          path: 'videoTransform',
+          name: 'videoTransform',
+          component: () => import('@/views/tools/VideoTransform.vue')
+        },
+        {
+          path: 'base64',
+          name: 'base64',
+          component: () => import('@/views/tools/Base64.vue')
+        },
+        {
+          path: 'docxpreview',
+          name: 'docxpreview',
+          component: () => import('@/views/tools/Docxpreview.vue')
+        }
+      ]
+    }
   ]
 })
 
-router.beforeEach((to,from,next)=>{
-  console.log(to,from)
-  let routes = router.getRoutes();
-  if(routes.length > 1){
-    next();
+router.beforeEach((to, from, next) => {
+  console.log(to, from)
+  let routes = router.getRoutes()
+  if (routes.length > 1) {
+    next()
   } else {
-    let localRoutes = localStorage.getItem('routes');
-    if(localRoutes){
-      router.addRoute(JSON.parse(localRoutes));
-      router.push('/');
+    let localRoutes = localStorage.getItem('routes')
+    if (localRoutes) {
+      router.addRoute(JSON.parse(localRoutes))
+      router.push('/')
     } else {
-      next('/');
+      next('/')
     }
   }
-});
+})
 
 export default router
