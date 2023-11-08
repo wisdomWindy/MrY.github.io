990bd4a7d19645d63a1b42a681235925b06f7163
wisdomwindy <1659140773@qq.com>
2023-11-08 10:24:42 +0800

    配置按需加载

---
+++ docs/assets/Base64.ad062dca.1699410223303.js
@@ -0,0 +1,1 @@
+import{r}from"./index.10fe20ae.1699410223303.js";import{A as s,B as o}from"./vue.a647cbcc.1699410223303.js";import"./pinia.2126c78a.1699410223303.js";import"./vueRouter.685b0b79.1699410223303.js";const t={class:""};const e=r({},[["render",function(r,e){return s(),o("div",t)}]]);export{e as default};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
-import VueComponents from 'unplugin-vue-components/vite';
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueComponents from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-const timeStamp = new Date().getTime();
+const timeStamp = new Date().getTime()
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers:[ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()],
       imports: ['vue', 'vue-router', 'pinia'],
       dts: true
     }),
@@ -28,10 +28,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks:{
-          vue:['vue'],
-          pinia:['pinia'],
-          vueRouter:['vue-router']
+        manualChunks: {
+          vue: ['vue'],
+          pinia: ['pinia'],
+          vueRouter: ['vue-router']
         },
         dir: 'docs',
         chunkFileNames: `assets/[name].[hash].${timeStamp}.js`,
@@ -39,17 +39,17 @@ export default defineConfig({
         assetFileNames: `assets/[name].[hash].${timeStamp}.[ext]`
       }
     },
-    cssCodeSplit:true,
-    cssMinify:true,
-    minify:'terser',
-    terserOptions:{
-      compress:{
-        drop_console:true,
-        drop_debugger:true
+    cssCodeSplit: true,
+    cssMinify: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       },
-      format:{
-        comments:false,
-        beautify:false
+      format: {
+        comments: false,
+        beautify: false
       }
     }
   }
